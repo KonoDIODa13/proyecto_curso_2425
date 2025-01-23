442c83777b807139dab015db1bc4f23f17cc6ad6
jaime.gonbra <jaime.gonbra@educa.jcyl.es>
2025-01-23 13:21:20 +0100

    completado todo el tema de crud basico con apis de python

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,50 +1,69 @@
 from fastapi import APIRouter, Depends
-from app.schemas import User, UserId
+from app.schemas import User, UserId, ShowUser, UpdateUser
 from app.db.database import get_db
 from sqlalchemy.orm import Session
 from app.db import models
+from typing import List
 
 router= APIRouter(
     prefix="/user",
     tags=["Users"]
 )
-usuarios= [] # esto es un dicionario cojone, no una lista
 
-@router.get("/")
-def obtener_usuarios(db:Session=Depends(get_db)):
+@router.get("/", response_model=List[ShowUser])
+def getUsers(db:Session=Depends(get_db)):
     data = db.query(models.User).all()
-    print(data)
-    return usuarios
+    return data
 
+@router.get("/{id}", response_model=ShowUser)
+def getUserByID(id:int, db:Session=Depends(get_db)):
+    usuario= db.query(models.User).filter(models.User.id==id).first()
+    if not usuario:
+        return {"Respuesta": "Usuario no encontrado."}
+    else:
+        return usuario
 
-@router.get("/ruta1")
-def ruta1():
-    return {"mensaje": "primmer commit"}
+@router.delete("/{id}/delete")
+def eliminar_usuario(id: int, db:Session=Depends(get_db)):
+    usuario= db.query(models.User).filter(models.User.id==id).first()
+    if not usuario:
+        return {"Respuesta": "Usuario no encontrado."}
+    else:
+        db.delete(usuario)
+        db.commit()
+        return {"Respuesta": "Usuario eliminado con exito."}
 
-
-@router.get("/usuarios")
-def getUsers():
-    
-   return usuarios
-
-@router.get("/usuario/{id}")
-def getUserByID(id:int):
-    for usuario in usuarios:
-        if usuario["id"]==id:
-            print(usuario, type(user))
-            return {"usuario": usuario}
-    return{"Respuesta": "Usuario no encontrado"}
-@router.delete("/delete/usuario/{id}")
-def eliminar_usuario(id: int):
-    # NECESITAMOS SABER EL INDICE Y EL VALOR PARA VER SI EL id = AL QUE ESTAMOS RECORRIENDO
-    for index, user in enumerate(usuarios):
-        if user["id"] == id:
-            usuarios.pop(index)
-            return {"Respuesta": "Usuario eliminado correctamente"}
-    return {"Respuesta": "Usuario NO encontrado"}
+@router.patch("/{id}")
+def modificar_usuario(id: int, updateUser: UpdateUser, db:Session=Depends(get_db)):
+    usuario = db.query(models.User).filter(models.User.id==id)
+    if not usuario.first():
+        return {"Respuesta": "Usuario no encontrado."}
+    usuario.update(updateUser.model_dump(exclude_unset=True))
+    db.commit()
+    return {"Respuesta": "Usuario modificado con exito."}
 
 @router.post("/crear_usuario")
-def addUser(user: User):
+def addUser(user: User, db:Session=Depends(get_db)):
     usuario= user.model_dump()
-    usuarios.append(usuario)
-    return {"Respuesta": "usuario creado!"}
+    nuevo_usuario= models.User(
+        username = usuario["username"],
+        password = usuario["password"],
+        nombre = usuario["nombre"],
+        apellido = usuario["apellido"],
+        direccion = usuario["direccion"],
+        telefono = usuario["telefono"],
+        correo = usuario["correo"],
+    )
+    data = db.query(models.User).all()
+    existe= False
+    for dataUser in data:
+        # print("usuario data"+str(dataUser.username)) asi si joe
+        if dataUser.username == nuevo_usuario.username:
+            existe=True
+    if existe:
+        return {"Respuesta": "error al crear usuario."}
+    else: 
+        db.add(nuevo_usuario)
+        db.commit()
+        db.refresh(nuevo_usuario)
+        return {"Respuesta": "usuario creado!"}
--- app/schemas.py
+++ app/schemas.py
@@ -4,16 +4,34 @@ from pydantic import BaseModel
 
 
 class User(BaseModel):
-    id: int
+    #id: int
+    username: str
+    password: str
     nombre: str
     apellido: str
     direccion: Optional[str]
     telefono: int
+    correo: str
     creacion_user: datetime = datetime.now()
 
 class UserId(BaseModel):
     id:int
 
+
+class ShowUser(BaseModel):
+    username: str
+    nombre:str
+    correo:str
+
+
+class UpdateUser(BaseModel):
+    username: str = None
+    password: str = None
+    nombre: str = None
+    apellido: str = None
+    direccion: str = None
+    telefono: int = None
+    correo: str = None
 
     """
     Usuario de prueba:
